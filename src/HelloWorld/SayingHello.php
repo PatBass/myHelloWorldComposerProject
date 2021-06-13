b0ebc4067b10366799e207d8eb3aa31cd772c127
PatrickBassoukissa <patrickbassoukissa@yahoo.fr>
2021-06-13 10:16:59 +0200

    First commit

---
+++ src/HelloWorld/SayingHello.php
@@ -0,0 +1,9 @@
+<?php 
+namespace HelloWorld;
+class SayingHello
+{
+    public static function world()
+    {
+        return "Hello World, I'm new to Packagist!";
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '529bdd9672173768beaeae7659f7414a7c3a5223',
+    'name' => 'patrickbassoukissa/hello-world-test-project',
+  ),
+  'versions' => 
+  array (
+    'patrickbassoukissa/hello-world-test-project' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '529bdd9672173768beaeae7659f7414a7c3a5223',
+    ),
+  ),
+);
